61837d7ea234457c3674c928dd77dcfe8f02a719
gleitons <31367377+gleitons@users.noreply.github.com>
2021-10-20 16:10:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function verImagem() {
+    AgoraAguarde.innerHTML = `Agora Aguarde!`
+    euTedisse.innerHTML = `Carregando...`
+    bemPesada.innerHTML = `Eu te Disse, ela é bem Pesada...`
+    var  img = document.querySelector('img#imagemPesada')
+    img.setAttribute('src', '/imagem/bem-pesasa.gif') 
+   
+    
+}
