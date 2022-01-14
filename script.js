4496e87510f689dfc045c9415c95b64edf5401bd
gleitons <31367377+gleitons@users.noreply.github.com>
2022-01-14 07:26:49 -0300

    atualiza

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function verImagem() {
-    AgoraAguarde.innerHTML = `Agora Aguarde!`
-    euTedisse.innerHTML = `Carregando...`
-    bemPesada.innerHTML = `Eu te Disse, ela é bem Pesada...`
-    var  img = document.querySelector('img#imagemPesada')
-    img.setAttribute('src', 'imagem/bem-pesasa.gif') 
+// function verImagem() {
+//     AgoraAguarde.innerHTML = `Agora Aguarde!`
+//     euTedisse.innerHTML = `Carregando...`
+//     bemPesada.innerHTML = `Eu te Disse, ela é bem Pesada...`
+//     var  img = document.querySelector('img#imagemPesada')
+//     img.setAttribute('src', 'imagem/bem-pesasa.gif') 
    
     
-}
+// }
 function randomImagen() {
 
     const nAleatorio = (Math.random() * 68).toFixed(0,2);
 
 
 
-    imagemPesada.innerHTML = `<img class="imgN" src="/imagem/lo.gif" alt="">`
+    imagemPesada.innerHTML = `<img class="imgN" src="/random/${nAleatorio}.webp" alt="">`
 
 }
 function outraideia() {
