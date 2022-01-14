7469927e7af17ae204053d94af23b7b0ebc39208
gleitons <31367377+gleitons@users.noreply.github.com>
2022-01-14 12:14:27 -0300

    gogole

--- script.js
+++ script.js
@@ -11,8 +11,18 @@ function randomImagen() {
 
     const nAleatorio = (Math.random() * 68).toFixed(0,2);
     var  trocaImage = document.querySelector('img#trocaImage')
-    trocaImage.setAttribute('src', `random/${nAleatorio}.webp`)
 
+    const selecaoStick = (Math.random() * 4).toFixed(0,2)
+
+    if (selecaoStick == 1){
+        trocaImage.setAttribute('src', `random/${nAleatorio}.webp`)
+    } else if (selecaoStick == 2){
+        trocaImage.setAttribute('src', `random/bebe_${nAleatorio}.webp`)
+    } else if (selecaoStick == 3){
+        trocaImage.setAttribute('src', `random/meme_${nAleatorio}.webp`)
+    } else {
+        trocaImage.setAttribute('src', `random/animals_${nAleatorio}.webp`)
+    }
 
 
     imagePesada.innerHTML = `${imrandom}`
