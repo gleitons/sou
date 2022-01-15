232b975dbed611dc55f1b83b99b3c67b26c32a65
gleitons <31367377+gleitons@users.noreply.github.com>
2022-01-15 15:40:23 -0300

    fra

--- script.js
+++ script.js
@@ -110,11 +110,11 @@ function randomImagenindex() {
 
 function randomImagen() {
     
-    const nAleatorio = (Math.random() * 68).toFixed(0,2);
+    const nAleatorio = (Math.random() * 119).toFixed(0,3);
     var  trocaImage = document.querySelector('img#trocaImage');    
 
     const selecaoStick = (Math.random() * 7).toFixed(0,2);
-    const frasesR = (Math.random() * 141).toFixed(0,2);   
+    const frasesR = (Math.random() * 247).toFixed(0,3);   
 
 
     const randomFrase = frases(parseInt(frasesR));
@@ -137,7 +137,7 @@ function randomImagen() {
         if (nAleatorio <= 118) {
             trocaImage.setAttribute('src', `random/bebe_${nAleatorio}.webp`);
         } else {
-            const nAleatorio = (Math.random() * 118).toFixed(0,2);
+            const nAleatorio = (Math.random() * 118).toFixed(0,3);
             trocaImage.setAttribute('src', `random/bebe_${nAleatorio}.webp`);
         }
         
@@ -154,7 +154,7 @@ function randomImagen() {
         if (nAleatorio <= 119) {
             trocaImage.setAttribute('src', `random/memegatos_${nAleatorio}.webp`)
         } else {
-            const nAleatorio = (Math.random() * 119).toFixed(0,2);
+            const nAleatorio = (Math.random() * 119).toFixed(0,3);
             trocaImage.setAttribute('src', `random/memegatos_${nAleatorio}.webp`)
         }
         
@@ -163,7 +163,7 @@ function randomImagen() {
         if (nAleatorio <= 119) {
             trocaImage.setAttribute('src', `random/memegatos2_${nAleatorio}.webp`);
         } else {
-            const nAleatorio = (Math.random() * 119).toFixed(0,2);
+            const nAleatorio = (Math.random() * 119).toFixed(0,3);
             trocaImage.setAttribute('src', `random/memegatos2_${nAleatorio}.webp`);
         }
     } 
@@ -489,6 +489,99 @@ function frases(Snumero) {
     "\"Realmente não funciono sobre pressão\", já sentindo o braço formigar.",
     "Como que pode dormir doze horas e acordar completamente cansado?",
     "Tô tranquilo. Sério, tô tranquilo MESMO. Sério, dessa vez vai ser diferente. Tranquilão aqui",
+    "Acabei de ver a pessoa mais inteligente quando estava na frente do espelho.",
+    " Eu perderia peso, mas odeio perder.",
+    " Eu sigo a citação, “Sempre seja verdadeiro consigo mesmo” porque eu apenas minto para os outros!",
+    " Eu gostaria de poder silenciar as pessoas na vida real.",
+    " Não procuro problemas. Os problemas geralmente me encontram.",
+    " Prezados problemas … Por favor, dê-me algum desconto … Sou seu cliente regular.",
+    " Siga meu conselho, eu não uso mesmo.",
+    " Nunca ria das escolhas de sua esposa. Você é um deles.",
+    " O amor pode ser cego, mas o casamento é uma verdadeira revelação.",
+    " Você nunca pode comprar amor, mas ainda assim terá que pagar por ele.",
+    " Sou tão pobre que não consigo nem prestar atenção!",
+    " Sou muito preguiçoso para deixar de ser preguiçoso.",
+    " Eu realmente quero trabalhar muito. Mas ser preguiçoso é muito divertido.",
+    " Se eu ganhasse o prêmio por preguiça, mandaria alguém buscar para mim.",
+    " Não sou preguiçoso, estou no modo de economia de energia.",
+    " Não sou preguiçoso, prefiro o termo “participação seletiva”.",
+    " Não sou preguiçoso, apenas muito relaxado.",
+    " Não estou fugindo do trabalho duro, tenho preguiça de correr.",
+    " A preguiça é a mãe de todos os maus hábitos, mas em última análise ela é mãe e devemos respeitá-la!",
+    " Nunca desista de seus sonhos, continue dormindo.",
+    " Comprar é uma arte. Eu sou um artista. Respeite, por favor.",
+    " Eu realmente preciso de um dia entre sábado e domingo.",
+    " Eu amo meu trabalho apenas quando estou de férias.",
+    " Sempre chego tarde ao escritório, mas compenso saindo mais cedo.",
+    " 3 palavras mais bonitas para uma mulher casada do que EU TE AMO: Sem Culinária Hoje.",
+    " Atrás de todo homem de sucesso há uma mulher surpresa.",
+    " Atrás de cada grande homem há uma mulher revirando os olhos.",
+    " O homem é a cabeça, mas a mulher é o pescoço. E ela pode virar a cabeça do jeito que quiser.",
+    " Você pode estar certo ou pode ser o marido.",
+    " Na minha casa eu sou o chefe, minha esposa é apenas quem toma as decisões.",
+    " Desculpas das mulheres: Sinto muito, mas foi sua culpa.",
+    " Sim, claro que sou atlético… Navego na Internet todos os dias.",
+    " Hoje de manhã quando eu estava dirigindo minha Ferrari, o alarme me acordou.",
+    " Quero que alguém me dê um empréstimo e depois me deixe sozinho.",
+    " Quanto mais rico você fica, mais cara se torna a felicidade.",
+    " Sempre sonhei em ser milionário como meu tio! Ele também está sonhando.",
+    " Falar sozinho porque sou meu próprio consultor.",
+    " Claro que falo comigo mesmo. Às vezes preciso de conselhos de especialistas.",
+    " Falo comigo mesmo porque gosto de lidar com uma classe melhor de pessoas.",
+    " Eu não preciso de um cabeleireiro, meu travesseiro me dá um novo penteado todas as manhãs.",
+    " Eu juro que meu travesseiro poderia ser um cabeleireiro. Sempre acordo com os penteados mais esquisitos.",
+    " Não sou louco, prefiro o termo mentalmente hilário.",
+    " A coisa mais difícil que já tentei foi ser normal.",
+    " Fora de minha mente. Volto em cinco minutos.",
+    " Olá! Whatsapp está me usando.",
+    "​​Ei, você está lendo meu status de novo?",
+    " A vida é muito curta. Não desperdice lendo meu status do Whatsapp.",
+    " Ei você, sim, estou falando com você, por que diabos você está lendo meu status?",
+    " Ei você, sim você. O único lendo isso. Quer saber um segredo? Você é linda. Nunca desista.",
+    " Se você está lendo isso, tenho certeza de que não tem nada para fazer na vida.",
+    " Então você está verificando meu status.",
+    " Meu “visto pela última vez em” foi apenas para verificar o seu “visto pela última vez em”.",
+    " Diga isso na minha cara, não através do seu status.",
+    " Pare de verificar meu status melhor se você tiver o seu próprio.",
+    " ESPERE! Você tem hora marcada para ver meu status.",
+    " Nem sempre disponível. Tente sua sorte.",
+    " Felicidade é quando “visto pela última vez em” muda para “online” e depois para “digitação”",
+    " Bateria fraca, perturbe mais tarde.",
+    " Pode parecer que não estou fazendo nada, mas na minha cabeça estou muito ocupado.",
+    " Não banque o estúpido comigo, sou melhor nisso!",
+    " Assim que param de falar com você, começam a falar sobre você.",
+    " A vida dá obstáculos, mas sou um atleta. Então é divertido.",
+    " A vida me ensinou muitas lições, mas também encerrei essas aulas.",
+    " Eu realmente deveria fazer algo com minha vida … talvez amanhã.",
+    " Fiz uma enorme lista de coisas para fazer hoje. Eu simplesmente não consigo descobrir quem vai fazer isso.",
+    " Quando uma porta se fecha, outra porta deve ser aberta, mas se isso não acontecer, entre pela janela.",
+    " Se o plano A falhar, lembre-se de que você tem 25 letras restantes.",
+    " Às vezes, a vida lhe dá uma segunda chance porque talvez na primeira vez você não estivesse pronto.",
+    " Sempre dê 100%, a menos que esteja doando sangue.",
+    " Fui lembrado de que meu tipo de sangue é SEJA POSITIVO!",
+    " Tenho sangue tipo O positivo, do qual me lembro por permanecer ‘otimista positivo’.",
+    " Eu nasci para ser pessimista. Meu tipo de sangue é B negativo.",
+    " Não é culpa do espelho se você não gosta do seu reflexo.",
+    " Sou apenas um espelho para você, Você é bom, eu sou o melhor, Você é ruim, eu sou o pior.",
+    " Não tenho tempo para odiar as pessoas que me odeiam porque estou ocupado amando as pessoas que me amam.",
+    " Venha viver no meu coração e não pague aluguel.",
+    " Caro stress, vamos terminar.",
+    " Eu gostaria de ter um botão de exclusão na minha vida. Para apagar algumas pessoas, algumas memórias e alguns sentimentos.",
+    " Eu gostaria de me desculpar com alguém a quem NÃO ofendi. Por favor, paciente, entrarei em contato com você em breve.",
+    " Ter um filho faz de você um pai; Tendo dois, você é um árbitro.",
+    " Alguém já percebeu que “ESTUDAR” é uma mistura de ESTUDO e MORRER?",
+    " Estou física, emocional e mentalmente exausto.",
+    " Fisicamente Mentalmente Emocionalmente CANSADO.",
+    " Vida: Além da gravidade, nada me mantém no chão.",
+    " Esperar até o filme começar a comer sua pipoca, a coisa mais difícil do mundo.",
+    " Toda vez que tiro uma foto fico com fome porque ouço ‘queijo’, então começo a pensar em um bom sanduíche de queijo.",
+    " Meus hobbies são café da manhã, almoço e jantar.",
+    " Uma boa risada e um sono longo são as duas melhores curas para qualquer coisa.",
+    " Nunca julgue o livro por seu filme.",
+    " É sempre a pessoa errada que lhe ensina as coisas certas na vida.",
+    " Eu sei quem eu sou, você não precisa explicar.",
+    " Eu sou uma doce menina lil, mas se você me deixa louco, lembre-se que sempre tenho um bolso cheio de loucos esperando para sair!!",
+    " Relaxe, é fim de semana, só não pisque ou tudo vai acabar."
 
 
     ];
